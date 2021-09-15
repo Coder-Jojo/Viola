c5adfde93ec712bcd2de0c5ffecfe7764b72877c
Coder-Jojo <nikhilchaudhary1243@gmail.com>
2021-09-16 01:40:09 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, Intents } = require("discord.js");
+require("dotenv").config();
+
+// Create a new client instance
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log(`logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+  msg.reply(msg.content);
+});
+
+client.login(process.env.TOKEN);
